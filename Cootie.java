36df45617e8fe54cef630bd61d19020fcda24fdf
Justalafano <Justalafano@gmail.com>
2022-07-12 11:11:01 -0600

    Complete cootie object & driver.

--- Cootie.java
+++ Cootie.java
@@ -1,22 +1,33 @@
-import java.util.ArrayList;
-
 public class Cootie {
    private String digits;
-   private String[] messages = new String[8];
-   
-   public Cootie(){
-      digits = "";
+   private String[] messages = new String[8];{
+      messages[0] = "It is certain.";
+      messages[1] = "Without a doubt.";
+      messages[2] = "Most likely.";
+      messages[3] = "Ask again later.";
+      messages[4] = "Cannot predict now.";
+      messages[5] = "Don't count on it.";
+      messages[6] = "My sources say no.";
+      messages[7] = "Very doubtful.";
    }
-   public String numToggle(int num){
-         if(num % 2 == 0){
+   private String message;
+   
+   //display digits based on length of color chosen
+   public String makeChoice(int num){   
+         if (num % 2 == 0){
             digits = ("1, 2, 5, 6");
          }
-         else if(num % 2 == 1){
+         else if (num % 2 == 1){
             digits = ("3, 4, 7, 8");
          }
          return digits;
    }
-
+   //display Cootie Catcher message based on number chosen
+   public String cootieMessage(int num){
+      message = messages[num];
+      
+      return message;
    }
+}
 
 
--- CootieCatcher.java
+++ CootieCatcher.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class CootieCatcher {
@@ -6,68 +7,121 @@ public class CootieCatcher {
 
       Cootie catcher = new Cootie();
 
-      String userQuestion = "";
       String userInput = "";
       String numDisplay = "";
+      int userNumPick = 0;
+      int numberConv = 0;
 
       boolean error = false;
+      boolean numCheck = false;
+      String userLoop = "";
 
       System.out.println("Use the Cootie Catcher to answer your question.");
 
-         System.out.println("What is your question?");
+      do{ //Loop user until Q is pressed
+         System.out.println("\n" + "What is your question?");
+         
+         in.nextLine(); 
 
-         userQuestion = in.nextLine();
+         do{ //Loop user if incorrect color/input is entered
+            try{
+               error = false;
 
-      do{
-         try{
-            error = false;
+               System.out.println("\n" + "What color do you pick? (red, green, blue, yellow)");
 
-            System.out.println("What color do you pick? (red, green, blue, yellow)");
+               userInput = in.nextLine();
 
-            userInput = in.nextLine();
-
-            if(userInput.equalsIgnoreCase("red")){
-               for(int i = 0; i < userInput.length() + 1; i++){
-                  numDisplay = catcher.numToggle(i);
-                  
-                  System.out.println(numDisplay);
+               //Toggle through Cootie Catcher numbers based on length of color chosen
+               if (userInput.equalsIgnoreCase("red")){
+                  for (int i = 0; i < userInput.length(); i++){
+                     numDisplay = catcher.makeChoice(i);
+                     
+                     System.out.println(userInput.charAt(i) + " " + numDisplay);
+                  }
+               }
+               else if (userInput.equalsIgnoreCase("green")){
+                  for (int i = 0; i < userInput.length(); i++){
+                     numDisplay = catcher.makeChoice(i);
+                     
+                     System.out.println(userInput.charAt(i) + " " + numDisplay);
+                  }
+               }
+               else if (userInput.equalsIgnoreCase("blue")){
+                  for (int i = 0; i < userInput.length(); i++){
+                     numDisplay = catcher.makeChoice(i);
+                     
+                     System.out.println(userInput.charAt(i) + " " + numDisplay);
+                  }
+               }
+               else if (userInput.equalsIgnoreCase("yellow")){
+                  for (int i = 0; i < userInput.length(); i++){
+                     numDisplay = catcher.makeChoice(i);
+                     
+                     System.out.println(userInput.charAt(i) + " " + numDisplay);
+                  }
+               }
+               else{
+                  throw new IllegalArgumentException("\n" + "Incorrect input");
                }
             }
-            else if(userInput.equalsIgnoreCase("green")){
-               for(int i = 0; i < userInput.length() + 1; i++){
-                  numDisplay = catcher.numToggle(i);
-                  
-                  System.out.println(numDisplay);
-               }
-            
-            }
-            else if(userInput.equalsIgnoreCase("blue")){
-               for(int i = 0; i < userInput.length() + 1; i++){
-                  numDisplay = catcher.numToggle(i);
-                  
-                  System.out.println(numDisplay);
-               }
-            
-            }
-            else if(userInput.equalsIgnoreCase("yellow")){
-               for(int i = 0; i < userInput.length() + 1; i++){
-                  numDisplay = catcher.numToggle(i);
-                  
-                  System.out.println(numDisplay);
-               }
-            }
-            else{
-            throw new IllegalArgumentException("Incorrect input");
-            }
-            System.out.println("\n" + "The Cootie Catcher landed on: " + numDisplay + ".");
-            
-            System.out.println("Please pick out of these numbers.");
-            
-         }
-         catch(Exception e){
-            error = true;
-         }
-      }while(error);
+            catch (Exception e){
+               error = true;
 
+               System.out.println(e.getMessage());
+            }
+         }while (error);
+
+         do{ //Loop user if incorrect number/input is entered
+            try{
+               numCheck = true;
+
+               System.out.println("\n" + "The Cootie Catcher landed on: " + numDisplay + ".");
+               
+               System.out.println("Please pick a number that the Cootie Catcher landed on.");
+
+               userNumPick = in.nextInt();
+            
+               // Check if user's choice matches Cootie Catcher numbers specified
+               for (int i = 0; i < numDisplay.length(); i += 3) {
+                  numberConv = Character.getNumericValue(numDisplay.charAt(i));
+                  
+                  if (userNumPick == numberConv){
+                     numCheck = false;
+                  }
+               }
+               if (numCheck == true){
+                  System.out.println("\n" + "Error, please try again.");
+               }
+            }
+            catch (InputMismatchException i){
+               in.nextLine(); // Flush buffer
+
+               numCheck = true;
+
+               System.out.println("\n" + "Error, a number must be entered.");
+            }
+            catch (Exception e){
+               in.nextLine(); // Flush buffer
+
+               numCheck = true;
+
+               System.out.println("\n" + e.getMessage());
+            }
+         
+         }while (numCheck);
+               
+         System.out.println("\n" + "The Cootie Catcher's answer is: " + 
+            catcher.cootieMessage(userNumPick-1));
+         
+         System.out.println("\n" + "Do you want to ask another question?" + 
+            "\n" + "Press any key to continue or Q to quit.");
+         
+         in.nextLine(); // Flush buffer
+
+         userLoop = in.nextLine();
+      
+      }while (!userLoop.equalsIgnoreCase ("Q"));
+
+      in.close();
    }
 }
